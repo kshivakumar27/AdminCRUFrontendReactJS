56a858c198e07a3718934935923e893f1110507c
Shiva Kumar K <shivakumar.k@ltts.com>
2022-05-08 10:26:43 +0530

    Updates

---
+++ precache-manifest.1803223cafee84bdc90274fa07607349.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75e03b018d52b3036c466a08183757c9",
+    "url": "/AdminCRUFrontendReactJS/index.html"
+  },
+  {
+    "revision": "8c5aba18c84ad27da6c5",
+    "url": "/AdminCRUFrontendReactJS/static/js/2.0a4ceb14.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/AdminCRUFrontendReactJS/static/js/2.0a4ceb14.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "80c92e34861a16667f72",
+    "url": "/AdminCRUFrontendReactJS/static/js/main.19f400bb.chunk.js"
+  },
+  {
+    "revision": "fc07e0b7e719ccbe7a88",
+    "url": "/AdminCRUFrontendReactJS/static/js/runtime-main.1b734f52.js"
+  }
+]);
